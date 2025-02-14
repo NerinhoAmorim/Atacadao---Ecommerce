5976cfb513d4f91a328479ebeb18bd1f88cabfdf
Nerinho Amorim Nascimento <156155459+NerinhoAmorim@users.noreply.github.com>
2025-02-13 23:49:33 -0300

    feat: implementação camada Token
    Signed-off-by: Nerinho Amorim Nascimento <156155459+NerinhoAmorim@users.noreply.github.com>

---
+++ src/main/java/br/com/atacadao/ecommerce/autenticacao/domain/Token.java
@@ -0,0 +1,17 @@
+package br.com.atacadao.ecommerce.autenticacao.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Token {
+	private String token;
+	private String tipo;
+
+}
